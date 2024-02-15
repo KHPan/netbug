bd1474653786c6d393e678fa5d045760703463a6
hank940115 <146960977+hank940115@users.noreply.github.com>
2024-02-15 17:41:52 +0800

    runCode測試成功

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     while True:
         url = input("url:")
         site = site_list.find(url)
-        if site is None:
-            site = netbug4.Site()
-        print(site.trans(url, is_test = True).prettify())
+        bs, _ = site.trans(url)
+        ans = netbug4.runCode(bs, site.fpreread)
+        print(ans)

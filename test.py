74c6b120b7b21814c2e312e117ea824b10679001
hank940115 <146960977+hank940115@users.noreply.github.com>
2024-03-01 19:46:22 +0800

    test with QT

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-import netbug4
+from netbug4 import *
+import sys
+from PyQt5 import QtWidgets, QtCore
 if __name__ == "__main__":
-	site_list = netbug4.SiteList()
+	app = QtWidgets.QApplication(sys.argv)
+	widget = QtWidgets.QWidget()
+	widget.resize(360, 360)
+	widget.setWindowTitle("hello, pyqt5")
+	widget.show()
+	sys.exit(app.exec_())
+	'''site_list = netbug4.SiteList()
 	url = "https://big5.quanben5.com/n/shouchangmishu/"#input("url:")
 	site = site_list.find(url)
-	netbug4.Test(site, url, site_list)
-	'''novel = netbug4.Novel(site, url)
-	print(f"novel name:{novel.novelName()}")
-	file_name = "test.txt"
-	novel.setFile(file_name)
-	for index, page in enumerate(novel.iterAll()):
-		page.text(index)'''
+	netbug4.Test(site, url, site_list)'''
